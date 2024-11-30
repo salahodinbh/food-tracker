c105c908f92d2d2abd91d3593848f7b5c3589a9e
Stryfura <shilunett@gmail.com>
2024-11-30 17:14:52 +0900

    Added okta login provider

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,40 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
+
+import Auth from '@okta/okta-vue'
+
+
 import Hello from '@/components/Hello'
+
+Vue.use(Auth, {
+  issuer: 'https://dev-rj26etfcudpvbpn0.us.auth0.com/oauth2/default',
+  client_id: 'C9CleDikynO5wVu11K9q9L6sPKIYYz2W',
+  redirect_uri: 'http://localhost:8080/implicit/callback',
+  scope: 'openid profile email'
+})
 
 Vue.use(Router)
 
-export default new Router({
+let router = new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'Hello',
       component: Hello
-    }
+    },
+    {
+      path: '/implicit/callback',
+      component: Auth.handleCallback()
+    },
   ]
 })
+
+router.beforeEach(Vue.prototype.$auth.authRedirectGuard())
+
+{
+  true
+}
+
+export default router
